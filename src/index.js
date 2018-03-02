4cfd00816fb08ecbb7e7a986cfc4670193740ac3
IohaRoman <ioxa147@gmail.com>
2018-03-02 16:32:46 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    if (str.length % 2 === 0){
+        for (var i = str.length-1; i >= 0 ; i--){
+            for(var j = 0; j < bracketsConfig.length; j++){
+                if (str[i] == bracketsConfig[j][0]) {
+                    str = str.replace(bracketsConfig[j].join(''),'');
+                }
+            }
+        }
+
+        if (str === ''){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
+
+};
